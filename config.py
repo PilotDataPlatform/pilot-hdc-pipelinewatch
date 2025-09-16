c11f9f8e720fdc9d034bdbd41794737a778f87e7
Vadym Moshynskyi <vmoshynskyi@indocresearch.org>
2025-09-16 16:22:03 +0200

    IEBH-452: Sync with the latest changes

--- config.py
+++ config.py
@@ -6,38 +6,9 @@
 
 import logging
 from functools import lru_cache
-from typing import Any
-from typing import Dict
-from typing import Optional
 
-from common import VaultClient
 from pydantic import BaseSettings
 from pydantic import Extra
-
-
-class VaultConfig(BaseSettings):
-    """Store vault related configuration."""
-
-    APP_NAME: str = 'pipelinewatch'
-    CONFIG_CENTER_ENABLED: bool = False
-
-    VAULT_URL: Optional[str]
-    VAULT_CRT: Optional[str]
-    VAULT_TOKEN: Optional[str]
-
-    class Config:
-        env_file = '.env'
-        env_file_encoding = 'utf-8'
-
-
-def load_vault_settings(settings: BaseSettings) -> Dict[str, Any]:
-    config = VaultConfig()
-
-    if not config.CONFIG_CENTER_ENABLED:
-        return {}
-
-    client = VaultClient(config.VAULT_URL, config.VAULT_CRT, config.VAULT_TOKEN)
-    return client.get_from_vault(config.APP_NAME)
 
 
 class Settings(BaseSettings):
@@ -58,10 +29,6 @@ class Settings(BaseSettings):
         env_file = '.env'
         env_file_encoding = 'utf-8'
         extra = Extra.allow
-
-        @classmethod
-        def customise_sources(cls, init_settings, env_settings, file_secret_settings):
-            return env_settings, init_settings, load_vault_settings, file_secret_settings
 
 
 @lru_cache(1)
